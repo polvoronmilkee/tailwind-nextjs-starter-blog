426d965d0b4d18dd9b88e1609556046b5a91f869
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2026-01-21 22:48:09 +0800

    chore: added app folder to storybook stories path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: [
-    // '../stories/**/*.mdx',
-    // '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs',
